0d1b089ebdc93e5300ff6147be788a732250aa9e
Waldanag <walter@algabits.com>
2023-08-15 17:24:56 -0400

    Avances parcial pto 1 y 2

--- parcialFE2Wal/consignas 3.0 - ESTUDIANTE/code.js
+++ parcialFE2Wal/consignas 3.0 - ESTUDIANTE/code.js
@@ -36,36 +36,67 @@ cambiarTema.addEventListener("click", alternarColorTema);
 function obtenerDatosDelUsuario() {
   /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
 
-let validacionNombre = "";
+let validacionTexto = "";
 do {
-  datosPersona.nombre = prompt(validacionNombre + "\n Ingresa tu nombre");
-  validacionNombre = esNombreValido(datosPersona.nombre);
-} while (validacionNombre !== true);
+  datosPersona.nombre = prompt(validacionTexto + "\n Ingresa tu nombre").toLowerCase();
+  validacionTexto = esTextoValido(datosPersona.nombre);
+} while (validacionTexto !== true);
+console.log(validacionTexto);
 
-/*const y = new Date().getFullYear();
-datosPersona.edad = y - parseInt( prompt("Ingresa tu año de nacimiento") );
-datosPersona.ciudad = prompt("Ingresa tu Ciudad");
-datosPersona.interesPorJs = confirm("Tiene interes por Javascript?");*/
-}
-
-function esNombreValido (nombre) {
-  if (nombre.length < 3 && isNaN(nombre)) {
-    return "Error, no es un nombre valido";
+let validacionAnio = "";
+do {
+  let anioNacimiento = prompt(validacionAnio + "\n Ingresa tu año de nacimiento");
+  validacionAnio = esAnioValido(anioNacimiento);
+  if (validacionAnio === true) {
+    datosPersona.edad = y - anioNacimiento;
   }
-  return true;
+} while (validacionAnio !== true);
+
+validacionTexto = "";
+do {
+  datosPersona.ciudad = prompt(validacionTexto + "\n Ingresa tu Ciudad").toLowerCase();
+  validacionTexto = esTextoValido(datosPersona.ciudad);  
+} while (validacionTexto !== true);
+
+datosPersona.interesPorJs = confirm("Tiene interes por Javascript?");
 }
 
-console.log(datosPersona);
-obtenerDatosDelUsuario()
+/*VALIDACIONES GENERALES DE LOS DATOS INGRESADOS*/
+function esTextoValido (texto) {
+  let str = texto.trim();
+  if (str.length > 3 && isNaN(texto)) {
+    return true;
+  }
+  return "Error, no es un dato valido";
+}
+const y = new Date().getFullYear();
+
+function esAnioValido(anio) {
+  if (!isNaN(anio) && anio < y && anio > 1900) {
+    return true;
+  }
+  return "Error, el formato del año debe ser con 4 números";
+}
 
 
 function renderizarDatosUsuario() {
   /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
   obtenerDatosDelUsuario();
   /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
-  
+  console.log(datosPersona);
+  let nombres = datosPersona.nombre.split(" ");
+  usuario = nombres.map((nombre) => nombre.charAt(0).toUpperCase() + nombre.slice(1));
 
+  let ciudad = datosPersona.ciudad.split(" ");
+  nombreCiudad = ciudad.map((ciudad) => ciudad.charAt(0).toUpperCase() + ciudad.slice(1));
 
+  const cardPersonalInfo = document.querySelector(".card-header");
+  cardPersonalInfo.innerHTML = `
+    <h3>Nombre: <span id="nombre">${usuario.join(" ")}</span></h3>
+    <h3>Edad: <span id="edad">${datosPersona.edad}</span></h3>
+    <h3>Ciudad: <span id="ciudad">${nombreCiudad.join(" ")}</span></h3>
+    <h3>Interes en Javascript: <span id="javascript">${datosPersona.interesPorJs ? "Si" : "No"}</span></h3>
+  `;
 }
 
 

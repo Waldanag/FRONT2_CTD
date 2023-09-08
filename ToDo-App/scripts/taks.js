77ee77f169321ab639dfe2b35b10487b6b35ca9c
Waldanag <walter@algabits.com>
2023-09-08 19:46:27 -0300

    Avances ToDo App

--- ToDo-App/scripts/taks.js
+++ ToDo-App/scripts/taks.js
@@ -1,24 +1,20 @@
 // SEGURIDAD: Si no se encuentra en localStorage info del usuario
 // no lo deja acceder a la página, redirigiendo al login inmediatamente.
 
-
-
 /* ------ comienzan las funcionalidades una vez que carga el documento ------ */
-window.addEventListener('load', function () {
-
+window.addEventListener("load", function () {
   /* ---------------- variables globales y llamado a funciones ---------------- */
-  
-
+  const btnCerrarSesion = document.getElementById("closeApp");
+  const url = "https://todo-api.ctd.academy/v1"
+  obtenerNombreUsuario();
 
   /* -------------------------------------------------------------------------- */
   /*                          FUNCIÓN 1 - Cerrar sesión                         */
   /* -------------------------------------------------------------------------- */
 
-  btnCerrarSesion.addEventListener('click', function () {
-   
-
-
-
+  btnCerrarSesion.addEventListener("click", function () {
+    localStorage.removeItem("jwt");
+    window.location.href = "index.html";
   });
 
   /* -------------------------------------------------------------------------- */
@@ -26,73 +22,83 @@ window.addEventListener('load', function () {
   /* -------------------------------------------------------------------------- */
 
   function obtenerNombreUsuario() {
-   
+    // Obtiene el token JWT (JSON Web Token) del almacenamiento local (localStorage)
+    const jwt = JSON.parse(localStorage.getItem("jwt"));
+    // Aquí se asume que el token JWT ya está guardado en localStorage
 
+    // Configura las opciones para la solicitud fetch
+    const settings = {
+        method: "GET", // Método HTTP GET
+        headers: {
+            'authorization': jwt, // Encabezado de autorización con el token JWT
+            'Content-Type': 'application/json' // Tipo de contenido de la solicitud
+        }
+    }
 
+    // Realiza una solicitud fetch a una URL específica (que debe estar definida previamente como 'url')
+    fetch(`${url}/users/getMe`, settings)
+    .then((response) => {
+        // Entra aquí cuando la solicitud fetch se completa (ya sea con éxito o no)
 
-  };
+        console.log({response}); // Muestra la respuesta en la consola
 
+        // Comprueba si la respuesta HTTP indica un error (por ejemplo, 404, 500)
+        if (!response.ok) {
+            if (response.status === 400) {
+                throw new Error(
+                    "Error 400: El usuario ya está registrado o datos requeridos incompletos"
+                );
+            } else if (response.status === 500) {
+                throw new Error("Error 500: Error del servidor");
+            } else {
+                throw new Error(
+                    `Error ${response.status}: Error desconocido, póngase en contacto con el administrador`
+                );
+            }
+        }
+
+        // Si la respuesta es exitosa, convierte los datos de la respuesta a JSON
+        return response.json();
+    })
+    .then((data) => {
+        // Entra aquí cuando la conversión a JSON se completa
+
+        console.log("Promesa cumplida💍"); // Muestra un mensaje en la consola
+
+        // Obtiene el nombre de usuario del objeto 'data' y lo almacena en 'nombreUsuario'
+        const nombreUsuario = data.firstName;
+    })
+
+    // Devuelve 'nombreUsuario' (Nota: 'nombreUsuario' solo estará definido dentro de la función 'then')
+    return nombreUsuario;
+}
 
   /* -------------------------------------------------------------------------- */
   /*                 FUNCIÓN 3 - Obtener listado de tareas [GET]                */
   /* -------------------------------------------------------------------------- */
 
   function consultarTareas() {
-    
-    
 
-
-
-  };
-
+  }
 
   /* -------------------------------------------------------------------------- */
   /*                    FUNCIÓN 4 - Crear nueva tarea [POST]                    */
   /* -------------------------------------------------------------------------- */
 
-  formCrearTarea.addEventListener('submit', function (event) {
-    
-
-
-
-
-  });
-
+  formCrearTarea.addEventListener("submit", function (event) {});
 
   /* -------------------------------------------------------------------------- */
   /*                  FUNCIÓN 5 - Renderizar tareas en pantalla                 */
   /* -------------------------------------------------------------------------- */
-  function renderizarTareas(listado) {
-
-
-
-
-
-
-
-  };
+  function renderizarTareas(listado) {}
 
   /* -------------------------------------------------------------------------- */
   /*                  FUNCIÓN 6 - Cambiar estado de tarea [PUT]                 */
   /* -------------------------------------------------------------------------- */
-  function botonesCambioEstado() {
-    
-    
-
-
-
-  }
-
+  function botonesCambioEstado() {}
 
   /* -------------------------------------------------------------------------- */
   /*                     FUNCIÓN 7 - Eliminar tarea [DELETE]                    */
   /* -------------------------------------------------------------------------- */
-  function botonBorrarTarea() {
-   
-    
-
-    
-
-  };
-
+  function botonBorrarTarea() {}
 });
